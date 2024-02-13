25cd83427c2d0bf9e6ce8112888db9326070d150
janae <ddlion15@byu.edu>
2024-02-12 21:01:21 -0700

    first half of milestone 5

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::controller(RegisterController::class)->group(function() {
+//     Route:post('register', 'register');
+//     Route:post('login','login');
+//     Route:post('logout','logout');
+// });
